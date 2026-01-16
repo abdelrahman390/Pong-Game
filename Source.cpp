66a45398469412c84d7f602bc60fa495f7b9f4d7
Abdelrahman <abdoelsayed1020@gmail.com>
2026-01-16 18:09:12 +0200

    Abdo task

--- Source.cpp
+++ Source.cpp
@@ -1,31 +1,45 @@
 ﻿#include <iostream>
 #include <raylib.h>
 #include <string>
+#include<algorithm>
+#include <raymath.h>
 using namespace std;
 
 
 struct Ball
 {
-    float x=400, y=250;
-    int speedx=7, speedy=7;
-    int radius=10;
+    float x = 400, y = 250;
+   float speedx = 7, speedy = 7;
+    int radius = 10;
+    int hitCount = 0;
+    int maxSpeed = 15; 
+    float prevX, prevY;
 
     void draw()
     {
         DrawCircle(x, y, radius, WHITE);
     }
 
-
     void update()
     {
+        prevX = x;
+        prevY = y;
+
+        speedy = Clamp(speedy, -9.0f, 9.0f);
         x += speedx;
         y += speedy;
 
-        if (y + radius >= GetScreenHeight() || y - radius <= 0)
+        if (y + radius >= GetScreenHeight())
         {
+            y = GetScreenHeight() - radius - 0.1f;
             speedy *= -1;
         }
-       
+        else if (y - radius <= 0)
+        {
+            y = radius + 0.1f; 
+            speedy *= -1;
+        }
+
     }
 
     void restball()
@@ -33,14 +47,96 @@ struct Ball
         x = GetScreenWidth() / 2;
         y = GetScreenHeight() / 2;
 
-        int speedchoices[2] = { -1,1 };
+        speedx = 6;
+        speedy = 6;
 
+        int speedchoices[2] = { -1, 1 };
         speedx *= speedchoices[GetRandomValue(0, 1)];
-        speedy *= speedchoices[GetRandomValue(0, 1)];
+           speedy *= speedchoices[GetRandomValue(0, 1)];
+        hitCount = 0;
+    }
+    
+    bool checkPaddleCollision(Vector2 paddleStart, Vector2 paddleEnd, float paddleThickness)
+    {
        
+        if (!CheckCollisionCircleLine({ x, y }, radius + fabs(speedx), paddleStart, paddleEnd))
 
+           
+    return false;
+
+
+        if ((speedx < 0 && x < paddleStart.x) ||
+            (speedx > 0 && x > paddleStart.x + paddleThickness))
+        {
+            return false; 
+        }
+        speedx *= -1;
+        hitCount++;
+        speedBall();
+
+      
+     
+        Vector2 movement = { x - prevX, y - prevY };
+
+        float length = sqrt(movement.x * movement.x + movement.y * movement.y);
+
+        if (length == 0) length = 0.001f;
+
+        movement.x /= length;
+        movement.y /= length;
+
+        while (CheckCollisionCircleLine({ x, y }, radius, paddleStart, paddleEnd))
+        {
+            x -= movement.x;
+            y -= movement.y;
+        }
+       
+        float paddleCenter = (paddleStart.y + paddleEnd.y) * 0.5f;
+        float paddleHeight = paddleEnd.y - paddleStart.y;
+        float distance = y - paddleCenter;
+        float normalized = distance / (paddleHeight * 0.5f);
+
+        if (normalized > 1.0f) normalized = 1.0f;
+        if (normalized < -1.0f) normalized = -1.0f;
+
+        float maxAngle = 40.0f * DEG2RAD; 
+        float angle = normalized * maxAngle;
+
+        float speed = sqrt(speedx * speedx + speedy * speedy);
+     
+        if (speedx> 0) {
+            speedx = 1 * cos(angle) * speed;
+        }
+        else {
+            speedx = -1 * cos(angle) * speed;
+        }
+        speedy = sin(angle) * speed;
+         
+            return true;
     }
 
+    void speedBall()
+    {
+        if (hitCount >= 4)
+        {
+            if (speedx > 0 && speedx < maxSpeed)
+            {
+                speedx++;
+            }
+            else if (speedx < 0 && speedx > -maxSpeed) 
+            {
+                speedx--;  
+            }
+            if (speedy > 0 && speedy < maxSpeed) {
+                speedy++;
+            }
+            else if (speedy < 0 && speedy > -maxSpeed) {
+                speedy--;
+            }
+
+            hitCount = 0;
+        }
+    }
 
 };
 
@@ -141,8 +237,9 @@ private:
         DrawText(("Player 1: " + to_string(score1)).c_str(), screen_width / 6, screen_height - 20, 20, border_color);
         DrawText(("Player 2: " + to_string(score2)).c_str(), screen_width / 6 * 4, screen_height - 20, 20, border_color);
 
-        DrawText(TextFormat("Ball Speed: %d", ball.speedy), 20, 20, 20, WHITE);
+       // DrawText(TextFormat("Ball Speed y: %.2f", ball.speedy), 20, 20, 20, WHITE);
 
+       // ext(TextFormat("Ball Speed x: %.2f", ball.speedx), 20, 50, 20, WHITE);
     }
 
 public:
@@ -183,11 +280,18 @@ public:
         int text_width = MeasureText(text, font_size);
         DrawText(text, screen_width / 2 - text_width / 4, screen_height / 2 - font_size / 2, font_size/2, border_color);
     }
+
+    
+
 };
 
 class Game 
 {
 private:
+
+  
+    Ball ball;
+
     int fps = 60;
     GameScreen screen;
     bool game_state = 0;
@@ -196,7 +300,6 @@ private:
 
     LeftPlayer lPlayer;
     rightPlayer rPlayer;
-
     int winner_score = 5;
 
     bool check_win()
@@ -236,8 +339,6 @@ private:
 
 public:
 
-    int hitCount = 0;
-    Ball ball;
     void run_game_loop()
     {
        
@@ -258,75 +359,33 @@ public:
                 screen.ball.update();
 
 
-                // line drawing size
-                const float paddleThickness = 5.0f;
-
-                // left racket line points
                 Vector2 lStart{ 20.0f, (float)lPlayer.getPaddleStart() };
                 Vector2 lEnd{ 20.0f, (float)lPlayer.getPaddleEnd() };
 
-                // Right racket line points
                 float rx = (float)GetScreenWidth() - 20.0f;
                 Vector2 rStart{ rx, (float)rPlayer.getPaddleStart() };
                 Vector2 rEnd{ rx, (float)rPlayer.getPaddleEnd() };
 
-                // Collision with the left
-                if (CheckCollisionCircleLine(Vector2{ screen.ball.x, screen.ball.y }, screen.ball.radius, lStart, lEnd)
-                    && screen.ball.speedx < 0)
-                {
-                    screen.ball.speedx *= -1;
-                   
-                //Move the ball out of front of the line so it doesn't get stuck
-                    screen.ball.x = 20.0f + paddleThickness * 0.5f + screen.ball.radius + 1.0f;
-                    hitCount++;
-                }
+                screen.ball.checkPaddleCollision(lStart, lEnd, 5.0f);
+                screen.ball.checkPaddleCollision(rStart, rEnd, 5.0f);
 
-                // Collision with the right
-                if (CheckCollisionCircleLine(Vector2{ screen.ball.x, screen.ball.y }, screen.ball.radius, rStart, rEnd)
-                    && screen.ball.speedx > 0)
-                {
-                    screen.ball.speedx *= -1;
-                  
-                    screen.ball.x = rx - paddleThickness * 0.5f - screen.ball.radius - 1.0f;
-                    hitCount++;
-                }
-               
-                //Increase the speed of the ball
-                if (hitCount >= 3)  
-                {
-                    
-                    if (screen.ball.speedx > 0)
-                        screen.ball.speedx += 1;
-                    else
-                        screen.ball.speedx -= 1;
+              
 
-                    if (screen.ball.speedy > 0)
-                        screen.ball.speedy += 1;
-                    else
-                        screen.ball.speedy -= 1;
-
-                    
-                    hitCount = 0;
-                }
-
-                if (screen.ball.x - screen.ball.radius <= 0)
+                if (screen.ball.x - screen.ball.radius <= -20)
                 {
                    
                     rPlayer.increaseScore();
                     screen.ball.restball();
-                    screen.ball.speedx = 7;
-                    screen.ball.speedy = 7;
+                   
                    
                 }
                 
                
-                if (screen.ball.x + screen.ball.radius >= GetScreenWidth())
-                    
+                if (screen.ball.x + screen.ball.radius >= GetScreenWidth()+20)
+
                 {
                     lPlayer.increaseScore();
                     screen.ball.restball();
-                    screen.ball.speedx = 7;
-                    screen.ball.speedy = 7;
                 }
                 
 
