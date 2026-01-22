91c2acd6c5a26c5b718928337b2f005b82b311bd
Abdelrahman Walid Elmarsafawy <88147091+abdelrahman390@users.noreply.github.com>
2026-01-22 15:37:05 +0200

    Refactor ball and player movement logic in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,25 @@
-﻿#include <iostream>
+#include <iostream>
 #include <raylib.h>
 #include <string>
-#include<algorithm>
+#include <algorithm>
 #include <raymath.h>
 using namespace std;
 
 
+const float FIXED_DT = 1.0f / 60.0f;
+float accumulator = 0.0f;
+
 struct Ball
 {
+    public:
+    float speedx = 400, speedy = 400;
     float x = 400, y = 250;
-   float speedx = 7, speedy = 7;
+
     int radius = 10;
     int hitCount = 0;
-    int maxSpeed = 15; 
+    int maxSpeed = 1000;
     float prevX, prevY;
+    float speedTimer = 0.0f;
 
     void draw()
     {
@@ -22,12 +28,26 @@ struct Ball
 
     void update()
     {
+        
+        // float dt = Clamp(dt, 1.0f / 120.0f, 1.0f / 60.0f);
+        float dt = GetFrameTime();
+
+        speedTimer += dt;
+
+        if (speedTimer >= 1.0f) {   // once per second
+            speedBall();
+            speedTimer = 0.0f;
+        }
+
         prevX = x;
         prevY = y;
 
-        speedy = Clamp(speedy, -9.0f, 9.0f);
-        x += speedx;
-        y += speedy;
+        speedy = Clamp(speedy, -600.0f, 600.0f);
+        x += speedx * dt;
+        y += speedy * dt;
+        // cout << prevX << " - " << prevY << endl;
+        // cout << x << " - " << y << endl;
+        // cout << " ---------------- " << endl;
 
         if (y + radius >= GetScreenHeight())
         {
@@ -36,10 +56,9 @@ struct Ball
         }
         else if (y - radius <= 0)
         {
-            y = radius + 0.1f; 
+            y = radius + 0.1f;
             speedy *= -1;
         }
-
     }
 
     void restball()
@@ -47,72 +66,73 @@ struct Ball
         x = GetScreenWidth() / 2;
         y = GetScreenHeight() / 2;
 
-        speedx = 6;
-        speedy = 6;
+        speedx = 400;
+        speedy = 400;
 
-        int speedchoices[2] = { -1, 1 };
+        int speedchoices[2] = {-1, 1};
         speedx *= speedchoices[GetRandomValue(0, 1)];
-           speedy *= speedchoices[GetRandomValue(0, 1)];
+        speedy *= speedchoices[GetRandomValue(0, 1)];
         hitCount = 0;
     }
-    
+
     bool checkPaddleCollision(Vector2 paddleStart, Vector2 paddleEnd, float paddleThickness)
     {
-       
-        if (!CheckCollisionCircleLine({ x, y }, radius + fabs(speedx), paddleStart, paddleEnd))
 
-           
-    return false;
+        if (!CheckCollisionCircleLine({x, y}, radius , paddleStart, paddleEnd))
 
+            return false;
 
         if ((speedx < 0 && x < paddleStart.x) ||
             (speedx > 0 && x > paddleStart.x + paddleThickness))
         {
-            return false; 
+            return false;
         }
         speedx *= -1;
         hitCount++;
         speedBall();
 
-      
-     
-        Vector2 movement = { x - prevX, y - prevY };
+        Vector2 movement = {x - prevX, y - prevY};
 
         float length = sqrt(movement.x * movement.x + movement.y * movement.y);
 
-        if (length == 0) length = 0.001f;
+        if (length == 0)
+            length = 0.001f;
 
         movement.x /= length;
         movement.y /= length;
 
-        while (CheckCollisionCircleLine({ x, y }, radius, paddleStart, paddleEnd))
+        while (CheckCollisionCircleLine({x, y}, radius, paddleStart, paddleEnd))
         {
             x -= movement.x;
             y -= movement.y;
         }
-       
+
         float paddleCenter = (paddleStart.y + paddleEnd.y) * 0.5f;
         float paddleHeight = paddleEnd.y - paddleStart.y;
         float distance = y - paddleCenter;
         float normalized = distance / (paddleHeight * 0.5f);
 
-        if (normalized > 1.0f) normalized = 1.0f;
-        if (normalized < -1.0f) normalized = -1.0f;
+        if (normalized > 1.0f)
+            normalized = 1.0f;
+        if (normalized < -1.0f)
+            normalized = -1.0f;
 
-        float maxAngle = 40.0f * DEG2RAD; 
+        float maxAngle = 40.0f * DEG2RAD;
         float angle = normalized * maxAngle;
 
         float speed = sqrt(speedx * speedx + speedy * speedy);
-     
-        if (speedx> 0) {
+
+        if (speedx > 0)
+        {
             speedx = 1 * cos(angle) * speed;
         }
-        else {
+        else
+        {
             speedx = -1 * cos(angle) * speed;
         }
         speedy = sin(angle) * speed;
-         
-            return true;
+
+        return true;
     }
 
     void speedBall()
@@ -121,59 +141,66 @@ struct Ball
         {
             if (speedx > 0 && speedx < maxSpeed)
             {
-                speedx++;
+                speedx += 50;
             }
-            else if (speedx < 0 && speedx > -maxSpeed) 
+            else if (speedx < 0 && speedx > -maxSpeed)
             {
-                speedx--;  
+                speedx -= 50;
             }
-            if (speedy > 0 && speedy < maxSpeed) {
-                speedy++;
+            if (speedy > 0 && speedy < maxSpeed)
+            {
+                speedy += 50;
             }
-            else if (speedy < 0 && speedy > -maxSpeed) {
-                speedy--;
+            else if (speedy < 0 && speedy > -maxSpeed)
+            {
+                speedy -= 50;
             }
 
             hitCount = 0;
         }
     }
-
 };
 
-
-class Player {
+class Player
+{
 protected:
-
-    int paddle_pos_start = GetScreenHeight() / 4;
-    int paddle_pos_end = GetScreenHeight() / 2;
-    int maximum_boundaries = GetScreenHeight();
+    float paddle_pos_start = GetScreenHeight() / 4;
+    float paddle_pos_end = GetScreenHeight() / 2;
+    float maximum_boundaries = GetScreenHeight();
     int score = 0;
-    int speed = 10;
+    int speed = 400;
 
 public:
     // If the value of c == u, this means going up, but if the value of c == d, this means going down.
-    void move_paddle(char c) {
-        if (c == 'u' && paddle_pos_start - 1 >= 0) {
-            paddle_pos_start = max(0, paddle_pos_start - speed);
-            paddle_pos_end-= speed;
+    void move_paddle(char c)
+    {
+        float dt = GetFrameTime();
+        if (c == 'u' && paddle_pos_start - 1 >= 0)
+        {
+            paddle_pos_start = max((float)0, paddle_pos_start - speed * dt);
+            paddle_pos_end -= speed * dt;
         }
-        else if (c == 'd' && paddle_pos_end + 1 <= maximum_boundaries) {
-            paddle_pos_end = min(maximum_boundaries, paddle_pos_end + speed);
-            paddle_pos_start+= speed;
+        else if (c == 'd' && paddle_pos_end + 1 <= maximum_boundaries)
+        {
+            paddle_pos_end = min(maximum_boundaries, paddle_pos_end + speed * dt);
+            paddle_pos_start += speed * dt;
         }
     }
 
     virtual void draw_paddle() = 0;
 
-    int getScore() {
+    int getScore()
+    {
         return score;
     }
 
-    void increaseScore() {
+    void increaseScore()
+    {
         score++;
     }
 
-    void resetScore() {
+    void resetScore()
+    {
         score = 0;
     }
 
@@ -186,70 +213,68 @@ public:
     {
         return paddle_pos_end;
     }
-
 };
 
-
-class LeftPlayer : public Player {
+class LeftPlayer : public Player
+{
 
 public:
-    void draw_paddle() {
-        // DrawLineEx({20, (float)GetScreenHeight() / 4}, {20, (float)GetScreenHeight() / 2}, 5, GREEN); 
-        DrawLineEx({ 20, (float)paddle_pos_start }, { 20, (float)paddle_pos_end }, 5, GREEN);
+    void draw_paddle()
+    {
+        // DrawLineEx({20, (float)GetScreenHeight() / 4}, {20, (float)GetScreenHeight() / 2}, 5, GREEN);
+        DrawLineEx({20, (float)paddle_pos_start}, {20, (float)paddle_pos_end}, 5, GREEN);
     }
-   
 };
 
-class rightPlayer : public Player {
+class rightPlayer : public Player
+{
 
 public:
-    void draw_paddle() {
-        DrawLineEx({ (float)GetScreenWidth() - 20, (float)paddle_pos_start }, { (float)GetScreenWidth() - 20, (float)paddle_pos_end }, 5, GREEN);
+    void draw_paddle()
+    {
+        DrawLineEx({(float)GetScreenWidth() - 20, (float)paddle_pos_start}, {(float)GetScreenWidth() - 20, (float)paddle_pos_end}, 5, GREEN);
     }
-    
-   
-
 };
 
-
-class GameScreen 
+class GameScreen
 {
 
 private:
-
-    Color bg_color = { 255, 255, 255, 255 };
+    Color bg_color = {255, 255, 255, 255};
     Color border_color = BLUE;
     float screen_width = 800;
     float screen_height = 500;
 
-
-    const char* game_title = "Ping Pong Game";
+    const char *game_title = "Ping Pong Game";
 
     void draw_borders()
     {
-        DrawLineEx({ 0, 0 }, { 0, screen_height }, 10, border_color);
-        DrawLineEx({ screen_width, 0 }, { screen_width, screen_height }, 10, border_color);
-        DrawLineEx({ screen_width / 2, 0 }, { screen_width / 2, screen_height }, 3, WHITE);
+        DrawLineEx({0, 0}, {0, screen_height}, 10, border_color);
+        DrawLineEx({screen_width, 0}, {screen_width, screen_height}, 10, border_color);
+        DrawLineEx({screen_width / 2, 0}, {screen_width / 2, screen_height}, 3, WHITE);
     }
 
-    void draw_score(int& score1, int& score2)
+    void draw_score(int &score1, int &score2)
     {
         DrawText(("Player 1: " + to_string(score1)).c_str(), screen_width / 6, screen_height - 20, 20, border_color);
         DrawText(("Player 2: " + to_string(score2)).c_str(), screen_width / 6 * 4, screen_height - 20, 20, border_color);
 
-       // DrawText(TextFormat("Ball Speed y: %.2f", ball.speedy), 20, 20, 20, WHITE);
+        // DrawText(TextFormat("Ball Speed y: %.2f", ball.speedy), 20, 20, 20, WHITE);
 
-       // ext(TextFormat("Ball Speed x: %.2f", ball.speedx), 20, 50, 20, WHITE);
+        // ext(TextFormat("Ball Speed x: %.2f", ball.speedx), 20, 50, 20, WHITE);
     }
 
 public:
     Ball ball;
     GameScreen()
     {
+        // ENABLE SMOOTHING FLAGS BEFORE InitWindow
+        // SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_VSYNC_HINT); 
+        SetConfigFlags(FLAG_MSAA_4X_HINT); 
         InitWindow(screen_width, screen_height, game_title);
     }
 
-    void draw_screen(int& score1, int& score2)
+    void draw_screen(int &score1, int &score2)
     {
         DrawFPS(screen_width - 100, 10);
         draw_borders();
@@ -260,11 +285,10 @@ public:
     {
         DrawFPS(screen_width - 100, 10);
         DrawText(TextFormat("Target FPS: %i (Use UP/DOWN to change)", fps), 10, 10, 20, WHITE);
-        const char* text = "Press Enter To Start!";
+        const char *text = "Press Enter To Start!";
         int font_size = 50;
         int text_width = MeasureText(text, font_size);
-        DrawText(text, screen_width / 2 - text_width / 2, screen_height / 2 - font_size , font_size, border_color);
-
+        DrawText(text, screen_width / 2 - text_width / 2, screen_height / 2 - font_size, font_size, border_color);
     }
 
     void draw_end_screen(int winner)
@@ -275,21 +299,16 @@ public:
         {
             text1 = "Game Resseted! Press Enter To Restart!";
         }
-        const char* text = text1.c_str();
+        const char *text = text1.c_str();
         int font_size = 50;
         int text_width = MeasureText(text, font_size);
-        DrawText(text, screen_width / 2 - text_width / 4, screen_height / 2 - font_size / 2, font_size/2, border_color);
+        DrawText(text, screen_width / 2 - text_width / 4, screen_height / 2 - font_size / 2, font_size / 2, border_color);
     }
-
-    
-
 };
 
-class Game 
+class Game
 {
 private:
-
-  
     Ball ball;
 
     int fps = 60;
@@ -337,15 +356,29 @@ private:
         }
     }
 
-public:
+    // void UpdatePhysics(float dt)
+    // {
+    //     screen.ball.x += screen.ball.speedx * dt;
+    //     screen.ball.y += screen.ball.speedy * dt;
+    // }
 
+public:
     void run_game_loop()
     {
-       
+
         SetTargetFPS(fps);
         ;
         while (!WindowShouldClose())
         {
+
+            // float frameTime = GetFrameTime();
+            // accumulator += frameTime;
+
+            // while (accumulator >= FIXED_DT)
+            // {
+            //     UpdatePhysics(FIXED_DT); // movement, collisions
+            //     accumulator -= FIXED_DT;
+            // }
 
             BeginDrawing();
             ClearBackground(BLACK);
@@ -355,40 +388,32 @@ public:
                 int rS = rPlayer.getScore();
 
                 screen.draw_screen(lS, rS);
-                screen. ball.draw();
                 screen.ball.update();
+                screen.ball.draw();
 
-
-                Vector2 lStart{ 20.0f, (float)lPlayer.getPaddleStart() };
-                Vector2 lEnd{ 20.0f, (float)lPlayer.getPaddleEnd() };
+                Vector2 lStart{20.0f, (float)lPlayer.getPaddleStart()};
+                Vector2 lEnd{20.0f, (float)lPlayer.getPaddleEnd()};
 
                 float rx = (float)GetScreenWidth() - 20.0f;
-                Vector2 rStart{ rx, (float)rPlayer.getPaddleStart() };
-                Vector2 rEnd{ rx, (float)rPlayer.getPaddleEnd() };
+                Vector2 rStart{rx, (float)rPlayer.getPaddleStart()};
+                Vector2 rEnd{rx, (float)rPlayer.getPaddleEnd()};
 
                 screen.ball.checkPaddleCollision(lStart, lEnd, 5.0f);
                 screen.ball.checkPaddleCollision(rStart, rEnd, 5.0f);
 
-              
-
                 if (screen.ball.x - screen.ball.radius <= -20)
                 {
-                   
+
                     rPlayer.increaseScore();
                     screen.ball.restball();
-                   
-                   
                 }
-                
-               
-                if (screen.ball.x + screen.ball.radius >= GetScreenWidth()+20)
+
+                if (screen.ball.x + screen.ball.radius >= GetScreenWidth() + 20)
 
                 {
                     lPlayer.increaseScore();
                     screen.ball.restball();
                 }
-                
-
 
                 lPlayer.draw_paddle();
                 rPlayer.draw_paddle();
@@ -398,8 +423,7 @@ public:
                     game_end = true;
                 }
 
-
-                // Move left paddale up 
+                // Move left paddale up
                 if (IsKeyPressed(KEY_W) || IsKeyDown(KEY_W))
                 {
                     lPlayer.move_paddle('u');
@@ -428,21 +452,12 @@ public:
                     game_end = true;
                 }
 
-                // when left player scores a goal 
-                // if(){
-                //     lPlayer.increaseScore();
-                // }
-
-                // when right player scores a goal 
-                // if(){
-                //     rPlayer.increaseScore();
-                // }
-
                 // press keypad 3 to reset scores
                 if (IsKeyPressed(KEY_KP_3))
                 {
                     reset_score();
                 }
+                
             }
             else if (game_state == 0 && game_end == false) // in menu
             {
@@ -454,27 +469,19 @@ public:
                     cout << "enter pressed" << endl;
                 }
 
-
-                // Move left paddale up 
-                if (IsKeyPressed(KEY_W) || IsKeyDown(KEY_W))
+                if (IsKeyPressed(KEY_DOWN))
                 {
-                    lPlayer.move_paddle('u');
-                }
-                // Move left paddale down
-                if (IsKeyPressed(KEY_S) || IsKeyDown(KEY_S))
-                {
-                    lPlayer.move_paddle('d');
+                    if (fps > 30)
+                    {
+                        fps -= 10;
+                        SetTargetFPS(fps);
+                    }
                 }
 
-                // Move right paddale up
-                if (IsKeyPressed(KEY_UP) || IsKeyDown(KEY_UP))
+                if (IsKeyPressed(KEY_UP))
                 {
-                    rPlayer.move_paddle('u');
-                }
-                // Move right paddale down
-                if (IsKeyPressed(KEY_DOWN) || IsKeyDown(KEY_DOWN))
-                {
-                    rPlayer.move_paddle('d');
+                    fps += 10;
+                    SetTargetFPS(fps);
                 }
 
                 // press backspace to reset game
@@ -483,16 +490,6 @@ public:
                     game_state = 0;
                     game_end = true;
                 }
-
-                // when left player scores a goal 
-                // if(){
-                //     lPlayer.increaseScore();
-                // }
-
-                // when right player scores a goal 
-                // if(){
-                //     rPlayer.increaseScore();
-                // }
             }
             else if (game_state == 0 && game_end == true) // end game / after game ended
             {
@@ -505,7 +502,7 @@ public:
                     reset_score();
                 }
             }
-            float ballSpeed = sqrt(ball.speedx * ball.speedx + ball.speedy * ball.speedy);
+            float ballSpeed = sqrt(screen.ball.speedx * screen.ball.speedx + screen.ball.speedy * screen.ball.speedy);
             EndDrawing();
         }
     }
